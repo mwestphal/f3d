45a56fb881477864a6a1a3d5e8aa450bf71ef772
mwestphal <mathieu.westphal@gmail.com>
2025-09-05 19:08:00 +0000

    Deploying to gh-pages from @ mwestphal/f3d@aa218bcb35fa2352e85f4a11025493abfecb036b 🚀

--- vtkext/private/module/vtkF3DImguiActor.cxx
+++ vtkext/private/module/vtkF3DImguiActor.cxx
@@ -283,12 +283,6 @@ void vtkF3DImguiActor::Initialize(vtkOpenGLRenderWindow* renWin)
 
   ImFontConfig fontConfig;
 
-  ImVector<ImWchar> ranges;
-  ImFontGlyphRangesBuilder builder;
-  builder.AddRanges(io.Fonts->GetGlyphRangesDefault());
-  builder.AddChar(0x2264); // Less-Than or Equal To
-  builder.BuildRanges(&ranges);
-
   ImFont* font = nullptr;
   if (this->FontFile.empty())
   {
@@ -296,11 +290,11 @@ void vtkF3DImguiActor::Initialize(vtkOpenGLRenderWindow* renWin)
     fontConfig.FontDataOwnedByAtlas = false;
     font = io.Fonts->AddFontFromMemoryTTF(
       const_cast<void*>(reinterpret_cast<const void*>(F3DFontBuffer)), sizeof(F3DFontBuffer), 18,
-      &fontConfig, ranges.Data);
+      &fontConfig);
   }
   else
   {
-    font = io.Fonts->AddFontFromFileTTF(this->FontFile.c_str(), 18, &fontConfig, ranges.Data);
+    font = io.Fonts->AddFontFromFileTTF(this->FontFile.c_str(), 18, &fontConfig);
   }
 
   io.Fonts->Build();
